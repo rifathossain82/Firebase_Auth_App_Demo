312aa2491e32d9a10e53ec7474a6e0b0ed639141
rifathossain82 <officialrifat82@gmail.com>
2023-01-08 16:45:57 +0600

    add fcm messeging by postman

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:firebase_auth_app_demo/src/view/screens/login_screen.dart';
 import 'package:firebase_auth_app_demo/src/view/screens/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+
+import 'src/services/service_2.dart';
+
+Future<void> backgroundHandler(RemoteMessage message) async {
+  print(message.data.toString());
+  print(message.notification!.title);
+}
 
 Future<void> main()async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(backgroundHandler);
+  LocalNotificationService.initialize();
   runApp(const MyApp());
 }
 
--- lib/src/view/screens/login_screen.dart
+++ lib/src/view/screens/login_screen.dart
@@ -1,8 +1,10 @@
 import 'package:firebase_auth_app_demo/src/auth/firebase_authentication.dart';
 import 'package:firebase_auth_app_demo/src/services/helper.dart';
+import 'package:firebase_auth_app_demo/src/services/service_2.dart';
 import 'package:firebase_auth_app_demo/src/view/screens/home_screen.dart';
 import 'package:firebase_auth_app_demo/src/view/widgets/k_button.dart';
 import 'package:firebase_auth_app_demo/src/view/widgets/k_text_field.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -19,10 +21,66 @@ class _LoginScreenState extends State<LoginScreen> {
   final FirebaseAuthentication firebaseAuthentication = FirebaseAuthentication();
 
   bool codeVisibility = false;
+  var deviceTokenToSendPushNotification = '';
+
+  Future<void> getDeviceTokenToSendNotification() async {
+    final FirebaseMessaging _fcm = FirebaseMessaging.instance;
+    final token = await _fcm.getToken();
+    deviceTokenToSendPushNotification = token.toString();
+    print("Token Value $deviceTokenToSendPushNotification");
+  }
+
+  @override
+  void initState() {
+    FirebaseMessaging.instance.subscribeToTopic("rh100");
+    FirebaseMessaging.instance.getInitialMessage().then(
+          (message) {
+        print("FirebaseMessaging.instance.getInitialMessage");
+        if (message != null) {
+          print("New Notification");
+          // if (message.data['_id'] != null) {
+          //   Navigator.of(context).push(
+          //     MaterialPageRoute(
+          //       builder: (context) => DemoScreen(
+          //         id: message.data['_id'],
+          //       ),
+          //     ),
+          //   );
+          // }
+        }
+      },
+    );
+
+    FirebaseMessaging.onMessage.listen(
+          (message) {
+        print("FirebaseMessaging.onMessage.listen");
+        if (message.notification != null) {
+          print(message.notification!.title);
+          print(message.notification!.body);
+          print("message.data11 ${message.data}");
+          LocalNotificationService.createanddisplaynotification(message);
+
+        }
+      },
+    );
+
+    FirebaseMessaging.onMessageOpenedApp.listen(
+          (message) {
+        print("FirebaseMessaging.onMessageOpenedApp.listen");
+        if (message.notification != null) {
+          print(message.notification!.title);
+          print(message.notification!.body);
+          print("message.data22 ${message.data['_id']}");
+        }
+      },
+    );
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
+    getDeviceTokenToSendNotification();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Login Screen'),
